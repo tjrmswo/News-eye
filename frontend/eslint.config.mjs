2304fe77ec173757cbc69540e0a00797893cab36
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2025-01-23 00:27:50 +0900

    chore: FE 환경 세팅

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,31 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import tailwind from 'eslint-plugin-tailwindcss';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...tailwind.configs['flat/recommended'],
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'import/order': ['warn', { 'newlines-between': 'always' }],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
+
+export default eslintConfig;
